8117ff11d79a3e137f0d59991a5e405775f3560b
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-07-27 18:55:35 -0700

    updated fav songs model

--- models/favoritesongs.js
+++ models/favoritesongs.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class favoritesongs extends Model {
+class favoritesongs extends Model {}
 
-User.init(
+favoritesongs.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -15,39 +15,7 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
-  },
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  
+  },  
 );
 
-module.exports = User;
+module.exports = favoritesongs;
