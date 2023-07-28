c554d6a7f3d3301b6f24523cacba39b42465652c
minkiko@gmail.com <minkiko@gmail.com>
2023-07-27 23:03:45 -0500

    test api

---
+++ controllers/api/index.js
@@ -0,0 +1,41 @@
+const API_KEY = 'AIzaSyB32ZnHIaVNrNZOcV1kz6YioSe4kJUUnJg'; // Aaron's YouTube API KEY
+
+
+function searchVideos() {
+    const query = document.getElementById('searchInput').value;
+    if (!query) return;
+
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
+        query + ' music clip'
+    )}&type=video&maxResults=10&key=${API_KEY}`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            displayResults(data.items);
+        })
+        .catch((error) => {
+            console.error('Error fetching videos', error);
+        });
+}
+
+
+function displayResults(items) {
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = '';
+
+    items.forEach((item) => {
+        const videoId = item.id.videoId;
+        const videoTitle = item.snippet.title;
+        const videoThumbnail = item.snippet.thumbnails.default.url;
+
+        const videoDiv = document.createElement('div');
+        videoDiv.innerHTML = `
+            <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
+                <img src="${videoThumbnail}" alt="${videoTitle}">
+                <p>${videoTitle}</p>
+            </a>
+        `;
+        resultsContainer.appendChild(videoDiv);
+    });
+}
