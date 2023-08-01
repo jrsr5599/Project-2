e5c72b41ee83645b657ed5ad2305570c2aaf4b3d
minkiko@gmail.com <minkiko@gmail.com>
2023-07-31 23:20:06 -0500

    initial start code for spotify web api

---
+++ controllers/api/app.js
@@ -0,0 +1,16 @@
+var redirect ='https://localhost:3001'
+
+var client_id = ' YOUR CLIENT ID';
+var client_secret = 'YOUR SECRET ID';
+
+const AUTHORIZE = 'https://accounts.spotify.com/authorize';
+
+const TOKEN = 'https://accounts.spotify.com/api/token'
+const ARTISTS = 'https://api.spotify.com/v1/me/top/artists?offset=0&limit=10&time_range=long_term';
+
+const list = document.getElementById('list');
+const cover = document.getElementById('cover');
+
+function authorize () {
+  
+}
